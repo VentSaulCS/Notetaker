e14125bb1931ae1e693e823470b06f1b29cbca2c
Saul Ventura <VentSaulCS@outlook.com>
2022-06-15 20:32:08 -0500

    added require

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const PORT = process.env.PORT || 8000;
+
+const app = express();
+
+app.use(express.urlencoded({extended:true}));
+app.use(express.json());
+app.use(express.static('public'));
+app.use(require('./routes/htmlRoutes.js'));
+app.use(require('./routes/apiRoutes'));
+
+
+
+app.listen(PORT, function(){
+    console.log('app is listening on, PORT')
+})
